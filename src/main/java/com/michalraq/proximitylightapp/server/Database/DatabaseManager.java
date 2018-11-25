70315da2b9e3eb206baa4d2d1ed600aeae7f1e9f
MichalR <raku9611@gmail.com>
2018-11-25 21:21:03 +0100

    Server_4 dodanie czytania z pliku danych do połaczenia

---
+++ src/main/java/com/michalraq/proximitylightapp/server/Database/DatabaseManager.java
@@ -0,0 +1,61 @@
+package com.michalraq.proximitylightapp.server.Database;
+
+import com.michalraq.proximitylightapp.server.Exceptions.LackOfDatabaseData;
+import com.michalraq.proximitylightapp.server.Util.FileReader;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class DatabaseManager {
+    private String hostName ;
+    private String dbName ;
+    private String user ;
+    private String password;
+    private String url ;
+    private Connection connection = null;
+
+   public DatabaseManager() throws LackOfDatabaseData {
+       ArrayList<String> list = FileReader.readDbFile("//database.txt");
+       if (!list.isEmpty()) {
+           hostName = list.get(0);
+           dbName = list.get(1);
+           user = list.get(2);
+           password = list.get(3);
+           url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
+       } else {
+           hostName = "";
+           dbName = "";
+           user = "";
+           password = "";
+           throw new LackOfDatabaseData();
+       }
+   }
+
+    public Boolean connectToDatabase(){
+        try {
+            connection = DriverManager.getConnection(url);
+            String schema = connection.getSchema();
+            System.out.println("Successful connection - Schema: " + schema);
+            System.out.println("Connection with database established");
+            return true;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public Boolean disconnectDatabase(){
+        try {
+            connection.close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        return false;
+        }
+    }
+
+}
---
+++ src/main/java/com/michalraq/proximitylightapp/server/Util/FileReader.java
@@ -0,0 +1,28 @@
+package com.michalraq.proximitylightapp.server.Util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileReader {
+
+
+    public static ArrayList<String> readDbFile(String filename){
+        File file = new File(filename);
+        Scanner in = null;
+        ArrayList<String> list = new ArrayList<String>();
+        try {
+            in = new Scanner(file);
+
+        while(in.hasNextLine()) {
+            list.add(in.nextLine());
+        }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File NOT FOUND !!!");
+            return null;
+        }
+        return list;
+    }
+}
