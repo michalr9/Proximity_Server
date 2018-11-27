20f7fbd88c28d67e22406e4172d0edcbd32234a2
MichalR <raku9611@gmail.com>
2018-11-28 00:01:47 +0100

    poprawienie maina

--- src/main/java/com/michalraq/proximitylightapp/server/Main.java
+++ src/main/java/com/michalraq/proximitylightapp/server/Main.java
@@ -1,24 +1,12 @@
 package com.michalraq.proximitylightapp.server;
 
-import com.michalraq.proximitylightapp.server.Database.DatabaseManager;
-import com.michalraq.proximitylightapp.server.Exceptions.LackOfDatabaseData;
-
 public class Main {
 
     public static void main(String[] args) {
-      //  Server server = new Server();
-        try {
-            DatabaseManager database = new DatabaseManager();
-            System.out.println("Url uzupelniono" + database.getUrl());
-database.connectToDatabase();
-database.disconnectDatabase();
-        }catch (LackOfDatabaseData e){
-            System.out.println("Wyjatek");
+        Server server = new Server();
+
+        while(true){
+            server.run();
         }
-
-
-//        while(true){
-//            server.run();
-//        }
     }
 }
